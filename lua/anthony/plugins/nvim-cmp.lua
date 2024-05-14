881a5578b7b3bd699ed87e7f5535553a5c1312cd
Ant Cole <acole@scileads.com>
2024-05-14 12:37:04 +0100

    feat(alpha-nvim.lua): update dashboard header for better readability feat(dadbod.lua): add new plugin vim-dadbod for database management feat(nvim-cmp.lua): add vim-dadbod-completion to nvim-cmp for SQL autocompletion feat(wakatime.lua): add new plugin vim-wakatime for time tracking

--- lua/anthony/plugins/nvim-cmp.lua
+++ lua/anthony/plugins/nvim-cmp.lua
@@ -52,5 +52,13 @@ return {
         }),
       },
     })
+
+    -- Setup up vim-dadbod
+    cmp.setup.filetype({ "sql" }, {
+      sources = {
+        { name = "vim-dadbod-completion" },
+        { name = "buffer" },
+      },
+    })
   end,
 }
